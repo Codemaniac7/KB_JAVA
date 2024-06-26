925c0a9a9b3dbeecd38281de6967b95f703bafe8
Jung Seung Min <jsm1592@gmail.com>
2024-06-26 20:15:11 +0900

    중첩

--- src/ch09/sec05/exam02/A.java
+++ src/ch09/sec05/exam02/A.java
@@ -24,7 +24,16 @@ public class A {
             //B 객체의 필드와 메소드 사용
             System.out.println(this.field);
             this.method();
-        }
 
+            //A 객체의 필드와 메소드 사용
+            System.out.println(A.this.field);
+            A.this.method();
+        }
+    }
+
+    //A의 인스턴스 메소드
+    void useB() {
+        B b = new B();
+        b.print();
     }
 }
