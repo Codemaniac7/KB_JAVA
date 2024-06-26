6331c36c04e0022c72c71c481bd749d73142bd3e
Jung Seung Min <jsm1592@gmail.com>
2024-06-26 17:59:09 +0900

    Exception

--- src/CLIFramework/domain/Lv1/App.java
+++ src/CLIFramework/domain/Lv1/App.java
@@ -8,4 +8,58 @@ public class App {
     int[] scores = null;
 
     Scanner scanner = new Scanner(System.in);
+
+    public void printMenu() {
+        System.out.println("----------------------------------");
+        System.out.println("1.학생수|2.점수입력|3.점수리스트|4.분석|5.종료");
+        System.out.println("----------------------------------");
+    }
+
+    public int getSelect() {
+        System.out.print("선택> ");
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    private void analize() {
+        int max = 0;
+        int sum = 0;
+        double avg = 0;
+        for(int i=0; i<scores.length; i++) {
+            max = (max<scores[i])? scores[i]: max;
+            sum += scores[i];
+        }
+        avg = (double) sum / studentNum;
+        System.out.println("최고 점수" + max);
+        System.out.println("평균 점수" + avg);
+    }
+    public void getStudentNum() {
+        System.out.print("학생수> ");
+        studentNum= Integer.parseInt(scanner.nextLine());
+        scores = new int[studentNum];
+    }
+
+    public void getScores() {
+        for(int i=0; i<scores.length; i++) {
+            System.out.print("scores["+i+"] = " + scores[i]);
+            scores[i] = Integer.parseInt(scanner.nextLine());
+        }
+    }
+    public void printScore() {
+        for(int i=0; i<scores.length; i++) {
+            System.out.println("scores["+i+"] = " + scores[i]);
+        }
+    }
+    public void exit() {
+        run = false;
+    }
+
+    public void executeCommand(int SelectNo) {
+//        if (selectNo == 1) {
+//            getStudentNum();
+//        } else if (selectNo==2) {
+//            getScores();
+//        } else if(selectNo==3) {
+//            printScore();
+//        }
+    }
 }
