a29ccc462c8c064e0abbdd71e4f9d95c92d4b654
Jung Seung Min <jsm1592@gmail.com>
2024-07-02 17:54:11 +0900

    collection

---
+++ src/ch15/sec04/exam01/HashMapExample.java
@@ -0,0 +1,37 @@
+package ch15.sec04.exam01;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class HashMapExample {
+    public static void main(String[] args) {
+        // Map컬렉션 생성
+        Map<String, Integer> map = new HashMap<String, Integer>();
+
+        // 객체 저장
+        map.put("신용권", 85);
+        map.put("홍길동", 90);
+        map.put("동장군", 80);
+        map.put("홍길동", 95);
+        System.out.println("총 Entry 수: " + map.size());
+        System.out.println();
+
+        // 키로 값 얻기
+        String key = "홍길동";
+        int value = map.get(key);
+        System.out.println(key + ": " + value);
+        System.out.println();
+
+        // 키 Set 컬렉션을 얻고, 반복해서 키와 값을 얻기
+        Set<String> keySet = map.keySet();
+        Iterator<String> keyIterator = keySet.iterator();
+        while (keyIterator.hasNext()) {
+            String k = keyIterator.next();
+            Integer v = map.get(k);
+            System.out.println(k + ": " + v);
+        }
+        System.out.println();
+    }
+}
