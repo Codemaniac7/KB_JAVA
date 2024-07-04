536c2b6669cfb1c582ddb75aacddd5aba9c1abff
Jung Seung Min <jsm1592@gmail.com>
2024-07-05 06:35:22 +0900

    optionalStream

--- src/ch17/sec10/OptionalExample.java
+++ src/ch17/sec10/OptionalExample.java
@@ -2,13 +2,25 @@ package ch17.sec10;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.OptionalDouble;
 
 public class OptionalExample {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
 
-        double avg = list.stream().mapToInt(Integer::intValue).average().getAsDouble();
 
-        System.out.println("평균: "+ avg);
+        OptionalDouble optional = list.stream().mapToInt(Integer::intValue).average();
+        if(optional.isPresent()) {
+            System.out.println("방법_1 평균: "+ optional.getAsDouble());
+        } else {
+            System.out.println("방법_1 평균: 0.0");
+        }
+
+        double avg = list.stream().mapToInt(Integer::intValue).average().orElse(0.0);
+        System.out.println("방법_2 평균: " + avg);
+
+        list.stream().mapToInt(Integer::intValue).average().ifPresent(a -> System.out.println("방법3_평균: " + a));
+
+
     }
 }
